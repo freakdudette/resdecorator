fb1bb799ae740e9e9d1e97d6dce2098c71820980
rrdev <roman.reaboi@gmail.com>
2018-10-27 11:33:40 +0300

    Add calligraphy

--- sample/src/main/java/digital/bakehouse/resdecorator/sample/SampleApp.java
+++ sample/src/main/java/digital/bakehouse/resdecorator/sample/SampleApp.java
@@ -3,11 +3,30 @@ package digital.bakehouse.resdecorator.sample;
 import android.app.Application;
 
 import digital.bakehouse.resdecorator.ResourceContextWrapper;
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
 
 public class SampleApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //Default usage
         ResourceContextWrapper.initialize();
+
+        //Using with calligraphy
+//        initializeWithCalligraphy();
+    }
+
+    private void initializeWithCalligraphy() {
+        ResourceContextWrapper.initialize(
+                ViewPump.builder()
+                        .addInterceptor(new CalligraphyInterceptor(
+                                new CalligraphyConfig.Builder()
+                                        .setDefaultFontPath("fonts/ArialMT.ttf")
+                                        .setFontAttrId(R.attr.fontPath)
+                                        .build())
+                        ));
     }
 }
